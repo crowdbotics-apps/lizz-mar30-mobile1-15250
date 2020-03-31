948cc27edfbb8e984b8b2a9aca14b93be913087b
Crowdbotics <team@crowdbotics.com>
2020-03-31 01:08:16 +0000

    Initial commit.

---
+++ backend/lizz_mar30_mobile1_15250/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for lizz_mar30_mobile1_15250 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lizz_mar30_mobile1_15250.settings')
+
+application = get_wsgi_application()
